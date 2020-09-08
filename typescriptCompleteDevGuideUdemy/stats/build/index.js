fd6d2651946055ffa39cb3b22a800876f86e6611
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-09-08 18:15:27 +0100

    add enum

--- typescriptCompleteDevGuideUdemy/stats/build/index.js
+++ typescriptCompleteDevGuideUdemy/stats/build/index.js
@@ -4,24 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs")); // import file system rom node standard library to allow us to interact with the computers system.
-var matches = fs_1.default.readFileSync('football.csv', {
+/*
+* Data structure of football.csv:
+* date / homeTeam / awayTeam / homeScore / awayScore / winner / referee
+*/
+var matches = fs_1.default
+    .readFileSync('football.csv', {
     encoding: 'utf-8'
 })
     .split('\n') //by new line
     .map(function (row) {
-    return row.split(','); // split the row by comma between elements.
+    return row.split(','); //each line is a row, and we want to split that row by the commas on it.
 });
+var MatchResult = {
+    HomeWin: 'H',
+    AwayWin: 'A',
+    Draw: 'D',
+};
 var manUnitedWins = 0;
-/*
-* Data structure:
-* date / homeTeam / awayTeam / homeScore / awayScore / winner / referee
-*/
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') { // if man u are home home win
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') { // if man u are away and away win
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- typescriptCompleteDevGuideUdemy/stats/src/index.ts
+++ typescriptCompleteDevGuideUdemy/stats/src/index.ts
@@ -1,24 +1,32 @@
 import fs from 'fs'; // import file system rom node standard library to allow us to interact with the computers system.
 
-const matches = fs.readFileSync('football.csv', { 
-  encoding: 'utf-8'
-  })
-  .split('\n') //by new line
-  .map((row: string): string[] => { // the return type is an array of strings.
-  return row.split(','); // split the row by comma between elements.
-});
-
-let manUnitedWins = 0;
-
 /*
-* Data structure:
+* Data structure of football.csv:
 * date / homeTeam / awayTeam / homeScore / awayScore / winner / referee
 */
 
+const matches = fs
+  .readFileSync('football.csv', { //its currently one huge string that spans many lines
+  encoding: 'utf-8'
+  })
+  .split('\n') //by new line
+  .map((row: string): string[] => { 
+  return row.split(','); //each line is a row, and we want to split that row by the commas on it.
+});
+
+
+const MatchResult = {
+  HomeWin: 'H',
+  AwayWin: 'A',
+  Draw: 'D',
+};
+
+let manUnitedWins = 0;
+
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') { // if man u are home home win
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') { // if man u are away and away win
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }

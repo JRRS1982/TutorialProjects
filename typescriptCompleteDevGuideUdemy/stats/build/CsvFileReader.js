c8609cb96d872aefa1a0545ebc7de4726b8cc66c
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-09-10 07:36:17 +0100

    add generic class to csv reader

--- typescriptCompleteDevGuideUdemy/stats/build/CsvFileReader.js
+++ typescriptCompleteDevGuideUdemy/stats/build/CsvFileReader.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 var fs_1 = __importDefault(require("fs")); // import file system rom node standard library to allow us to interact with the computers system.
-var utils_1 = require("./utils");
 var CsvFileReader = /** @class */ (function () {
     function CsvFileReader(filename) {
         this.filename = filename;
@@ -18,20 +17,9 @@ var CsvFileReader = /** @class */ (function () {
         })
             .split('\n') //by new line
             .map(function (row) {
-            return row.split(','); //each line is a row, and we want to split that row by the commas on it.
+            return row.split(','); //each line/match is a row, and we want to split that row by the commas on it.
         })
             .map(this.mapRow);
-    };
-    CsvFileReader.prototype.mapRow = function (row) {
-        return [
-            utils_1.dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5],
-            row[6]
-        ];
     };
     return CsvFileReader;
 }());
--- typescriptCompleteDevGuideUdemy/stats/src/CsvFileReader.ts
+++ typescriptCompleteDevGuideUdemy/stats/src/CsvFileReader.ts
@@ -1,14 +1,12 @@
 import fs from 'fs'; // import file system rom node standard library to allow us to interact with the computers system.
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
-  data: MatchData[] = []; // using a tuple as the csv should be a fixed order
+export abstract class CsvFileReader<TypeOfData> {  // TypeOfData makes this a generic type so it can be used by other data types, not just match data
+  data: TypeOfData[] = []; // using a tuple as the csv should be a fixed order
 
   constructor(public filename: string) {}
-
+  abstract mapRow(row: string[]): TypeOfData;
+  
   read(): void {
     this.data = fs
     .readFileSync(this.filename, { //its currently one huge string that spans many lines
@@ -16,20 +14,8 @@ export class CsvFileReader {
     })
     .split('\n') //by new line
     .map((row: string): string[] => { 
-      return row.split(','); //each line is a row, and we want to split that row by the commas on it.
+      return row.split(','); //each line/match is a row, and we want to split that row by the commas on it.
     })
     .map(this.mapRow);
-  }
-   
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ];
   }
 }

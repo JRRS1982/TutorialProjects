12a5cfe7e9f600ba442d1d7c4020e58314085d04
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-09-29 17:40:00 +0100

    add more rough notes on decorators. Really rough.

--- typescriptCompleteDevGuideUdemy/features/decorators.ts
+++ typescriptCompleteDevGuideUdemy/features/decorators.ts
@@ -16,36 +16,59 @@
 
 
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boat color is ${this.color}`;
   }
   
   // @logError   // a decorator - so first argument IN THIS LOCATION would be the PROTOTYPE OF Boat...  second argument would be pilot()
   @logProblem('This is a custom error message') // a decorator factory - with this we can add a message to the decorator.
-  pilot(): void {
-    throw new Error();
-    console.log('swish');
+  
+  // OMG YOU CAN APPLY DECORATORS TO PARAMETERS TOO
+  pilot(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    }
+    console.log('nothing');
   }
 }
 
 /*
 a decorator - logProblem is this code wrapped in a function so that we can pass in an argument.
 */
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value;  // represents Boat.pilot() method
-  desc.value = function() {
-    try {
-      method();
-    } catch (e) {
-      console.log('Oooopps boat sunk');
-    }
-  }
+// function logError(target: any, key: string, desc: PropertyDescriptor): void {
+//   const method = desc.value;  // represents Boat.pilot() method
+//   desc.value = function() {
+//     try {
+//       method();
+//     } catch (e) {
+//       console.log('Oooopps boat sunk');
+//     }
+//   }
+// }
+
+/*
+ARRRRGGHHHHHH OK... SO YOU CAN APPLY DECORATORS TO PARAMETERS TOO.... YOU MAY NEED THIS AT SOME POINT TO KNOW WHAT THE ARGUMENTS ARE INTO A FUNCTION.
+target = BOAT is the prototype of Boat, not an instance of Boat... so it has the functions, but not properties.
+key = pilot function on Boat
+index = a number - the index of the argument that we are applying this decorator to.
+*/
+function parameterDecorator(target: Boat, key: string, index: number) {
+  console.log(key);
+  console.log(index);
 }
 
-// this will call pilot function, but as @logError decorator is in place we have intercepted the call and called or own private function.
-new Boat().pilot();
+/*
+*/
+function classDecorator(constructor: typeof Boat) {
+  
+}
+
+// this will call pilot function, but as @logError decorator is/was in place we have intercepted the call and called or own private function.
+// new Boat().pilot();  // to to call logProblem
 
 /*
 Below is a decorator factory, which we can use to inject messages to the above logError decorator... currently lofError() is hard coded and only has one message, 
@@ -61,4 +84,9 @@ function logProblem(errorMessage: string) {
       }
     }
   }
+}
+
+function testDecorator(target: any, key: string){
+  console.log(target);
+  console.log(key);
 }

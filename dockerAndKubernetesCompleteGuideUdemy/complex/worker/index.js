45b8e5ec83184b46744de1e6d57b36b8f588fbe7
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-10-12 16:40:14 +0100

    setup redis

---
+++ dockerAndKubernetesCompleteGuideUdemy/complex/worker/index.js
@@ -0,0 +1,28 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+// create a redis client.
+const redisClient = redis.createClient({
+  host: keys.redisHost, // using hose from keys file
+  port: keys.redisPort, // using the port from the keys file
+  retry_strategy: () => 1000 // if lose connection to server, try once every 1000 milliseconds.
+});
+
+// create a duplicate of client above.
+const sub = redisClient.duplicate();
+
+
+// the fibonacci recursive solution.
+function fib(index) {
+  if (index < 2) return 1; // if index less than two (i.e. 0 and 1)
+  return fib(index - 1) + fib(index - 2); // else add the value of the prior two together.
+}
+
+sub.on('message', (channel, message) => { // every time there is a new value in redis
+  redisClient.hset( // hset being hash
+    'values', // insert the new fib value into a hash called values.
+    message, // the key of the hash will be the index - the message that was submitted into the form.
+    fib(parseInt(message))); // calculate a new fib value.
+});
+
+sub.subscribe('insert'); // anytime a new value is inserted to redis
---
+++ dockerAndKubernetesCompleteGuideUdemy/complex/worker/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPost: process.env.REDIS_PORT
+};

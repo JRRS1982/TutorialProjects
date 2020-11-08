1320a8a8bdd54699a5a7e75334609a0859f20028
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-11-08 11:44:05 +0000

    add semanticui style sheet and search bar

---
+++ reactRedux/videos/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;

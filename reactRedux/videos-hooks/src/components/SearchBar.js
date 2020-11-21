9ecd960fad43436b4acb866a1cf9ec5cadf75180
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-11-21 15:34:55 +0000

    refactor searchBar to Functional from Class Component

--- reactRedux/videos-hooks/src/components/SearchBar.js
+++ reactRedux/videos-hooks/src/components/SearchBar.js
@@ -1,34 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
-
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value }); // set the search term as the value of what is in the input of the search bar
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
+  
+  const onInputChange = (event) => {
+    setTerm(event.target.value); // set the search term as the value of what is in the input of the search bar
   };
-
-  onFormSubmit = (event) => {
+  
+  const onSubmit = (event) => {
     event.preventDefault(); // stop the page from refreshing
-    // SO WHEN WE SUBMIT THE FORM BELOW ON THE SEARCH BAR THE PARENT COMPONENT IS TOLD WHAT THE SEARCH TERM IS
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   };
-
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+ 
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={onInputChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;

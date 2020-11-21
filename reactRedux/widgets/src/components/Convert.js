93e0d16146ead14c1d2ed71a1c948da6e8087799
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-11-21 11:45:19 +0000

    return the translated text with convert component

--- reactRedux/widgets/src/components/Convert.js
+++ reactRedux/widgets/src/components/Convert.js
@@ -10,17 +10,27 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+
   useEffect(() => {
-    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-      params: {
-        q: text, 
-        target: language.value,
-        key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-      }
-    })
+    const doTranslation = async () => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text, 
+          target: language.value,
+          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+        }
+      })
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation(); // invoked when we first mount the component, when language changes and when the text changes
   }, [language, text]);
 
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;

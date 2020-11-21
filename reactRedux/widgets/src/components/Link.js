d69c8d9d5dbf84283f099517b9d02ec090d2e074
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-11-21 13:58:11 +0000

    allow metaKey and ctrl key on link

--- reactRedux/widgets/src/components/Link.js
+++ reactRedux/widgets/src/components/Link.js
@@ -7,6 +7,11 @@ import React from "react";
  */
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    // clicking with mac special key or windows ctrl key you will open the link in a new tab
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault(); // stop a full page reload as we don't need to re-render the whole page when a user clicks between components.
     // window.history.pustState updates the url of the window with the href that we are currently viewing. Without this we are going to be viewing one component, but the url on the page may not be the same as that which we are viewing as we have prevented the default refresh of the page to reduce the events that take place, but some events such as updating the url should still take place.
     window.history.pushState({}, '', href);

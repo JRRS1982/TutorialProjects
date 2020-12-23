3dac3358e77e7a1f82d1b9bbef4feab87429e28d
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-12-23 14:10:03 +0000

    streams/client: if stream is users allow edit/delete

--- reactRedux/streams/client/src/components/streams/StreamList.js
+++ reactRedux/streams/client/src/components/streams/StreamList.js
@@ -7,10 +7,25 @@ class StreamList extends React.Component {
     this.props.getStreams();
   }
 
+  /**
+   * a helper function to 
+   */
+  renderAdmin(stream) {
+    if (stream.googleUserId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)} {/*render admin has to be shown before the i tag for semantic ui to work*/}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
             {stream.title}
@@ -36,7 +51,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) }; // turn object into an array
+  return { 
+    streams: Object.values(state.streams),  // turn object into an array
+    currentUserId: state.auth.googleUserId
+  }; 
 };
 
 export default connect(mapStateToProps, { getStreams })(StreamList);
